c8b08241b9906d94530e64177a8f62af89866031
18105770206@163.com <y.258917>
2019-06-22 11:25:25 +0800

    增加新页面
    修改新增收获地址页样式

--- pages/merchant/merchant.js
+++ pages/merchant/merchant.js
@@ -1,14 +1,11 @@
-// pages/address/insert_addr/insert_addr.js
+// pages/merchant/merchant.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
-    name: '此时此刻',
-    author: '许巍',
-    src: './newmsg.mp3',
+
   },
 
   /**
@@ -21,21 +18,8 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function (e) {
-    // 使用 wx.createAudioContext 获取 audio 上下文 context
-    this.audioCtx = wx.createAudioContext('myAudio')
-  },
-  audioPlay: function () {
-    this.audioCtx.play()
-  },
-  audioPause: function () {
-    this.audioCtx.pause()
-  },
-  audio14: function () {
-    this.audioCtx.seek(14)
-  },
-  audioStart: function () {
-    this.audioCtx.seek(0)
+  onReady: function () {
+
   },
 
   /**
@@ -78,10 +62,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
-  select_addr: function () {
-    wx.navigateTo({
-      url: '/pages/address/select_addr/select_addr',
-    })
   }
 })
